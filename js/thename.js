74b2203f3a0fa7f1edc8f63bd933f79514869c94
Hanu <99120539+0xhannyu@users.noreply.github.com>
2024-10-20 21:53:36 +0530

    Delete js/thename.js

--- js/thename.js
+++
@@ -1,7 +0,0 @@
-const words = ['Hannyu', '[REDACTED]', 'Hanu', 'SGFudQ==', '0xh', 'हन्नू'];
-const thenameWordElement = document.getElementById('thename');
-
-setInterval(() => {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
-  thenameWordElement.innerText = randomWord;
-}, 100);
